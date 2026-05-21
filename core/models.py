c125e65343bfd012d61b9e85bc6d3810266fafd8
Mirparvin Miriyev <miriyevmirparvin@gmail.com>
2026-05-21 23:51:52 +0200

    initialize portfolio deployment

---
+++ core/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    image = models.ImageField(upload_to='projects/')
+
+    technologies = models.CharField(max_length=300)
+
+    github_link = models.URLField(blank=True)
+
+    live_demo = models.URLField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+class Contact(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    email = models.EmailField()
+
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from .models import Project, Contact
+
+def home(request):
+
+    projects = Project.objects.all().order_by('-created_at')
+
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        Contact.objects.create(
+            name=name,
+            email=email,
+            message=message
+        )
+
+        return redirect('/')
+
+    return render(request, 'core/index.html', {
+        'projects': projects
+    })
